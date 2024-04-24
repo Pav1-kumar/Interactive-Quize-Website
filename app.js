c7db7d96bf78caf09a2049655bb52ef65b7a32ff
Pavan Kumar <49811722+Pav1-kumar@users.noreply.github.com>
2024-04-24 23:14:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const correctAnswers= ['B','B','B','C','B'];
+const form=document.querySelector('.quize-form');
+const result=document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+   e.preventDefault();
+
+   let score=0;
+   const userAnswers=[form.q1.value , form.q2.value ,form.q3.value ,form.q4.value, form.q5.value];
+
+   //check answers
+   userAnswers.forEach((answer, index) => {
+   if(answer===correctAnswers[index])
+   {
+      score+=20;
+   }
+   else{
+
+      
+   }
+   });
+
+   //results
+   scrollTo(0,0);  //window object or global object
+  // result.querySelector('span').textContent = `${score}%`;
+   result.classList.remove('d-none');
+
+   let output=0;
+   let timer= setInterval(()=> {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output===score)
+    {
+        clearInterval(timer);
+    }
+    else
+    {
+       output++;
+    }
+
+   },20)
+
+
+}
+
+
+
+
+);
