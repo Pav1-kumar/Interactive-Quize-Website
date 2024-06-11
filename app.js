4514c62fb24278bd0ff3ef94d7598300d711ff64
Pavan Kumar <49811722+Pav1-kumar@users.noreply.github.com>
2024-06-11 10:03:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers= ['B','B','B','C','B'];
+const correctAnswers= ['B','B','B','C','B','A','A','C','D','D'];
 const form=document.querySelector('.quize-form');
 const result=document.querySelector('.result');
 
@@ -6,13 +6,13 @@ form.addEventListener('submit', e => {
    e.preventDefault();
 
    let score=0;
-   const userAnswers=[form.q1.value , form.q2.value ,form.q3.value ,form.q4.value, form.q5.value];
+   const userAnswers=[form.q1.value , form.q2.value ,form.q3.value ,form.q4.value, form.q5.value ,form.q6.value ,form.q7.value ,form.q8.value,form.q9.value,form.q10.value];
 
    //check answers
    userAnswers.forEach((answer, index) => {
    if(answer===correctAnswers[index])
    {
-      score+=20;
+      score+=10;
    }
    else{
 
@@ -37,7 +37,7 @@ form.addEventListener('submit', e => {
        output++;
     }
 
-   },20)
+   },10)
 
 
 }
