75422300057b830964ec2b24ffb4c089a19c1daa
Pavan Kumar <49811722+Pav1-kumar@users.noreply.github.com>
2024-08-23 16:27:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
-const correctAnswers= ['B','B','B','C','B','A','A','C','D','D'];
-const form=document.querySelector('.quize-form');
-const result=document.querySelector('.result');
+
+const correctAnswers = ['B', 'B', 'B', 'C', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
    e.preventDefault();
 
-   let score=0;
-   const userAnswers=[form.q1.value , form.q2.value ,form.q3.value ,form.q4.value, form.q5.value ,form.q6.value ,form.q7.value ,form.q8.value,form.q9.value,form.q10.value];
+   let score = 0;
+   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
 
-   //check answers
+   // check answers
    userAnswers.forEach((answer, index) => {
-   if(answer===correctAnswers[index])
-   {
-      score+=10;
-   }
-   else{
-
-      
-   }
+      if (answer === correctAnswers[index]) {
+         score += 20;
+      }
    });
 
-   //results
-   scrollTo(0,0);  //window object or global object
-  // result.querySelector('span').textContent = `${score}%`;
-   result.classList.remove('d-none');
+   // show result
+   result.classList.remove('hidden');
+   scrollTo(0, 0);
 
-   let output=0;
-   let timer= setInterval(()=> {
-    result.querySelector('span').textContent = `${output}%`;
-    if(output===score)
-    {
-        clearInterval(timer);
-    }
-    else
-    {
-       output++;
-    }
-
-   },10)
-
-
-}
-
-
-
-
-);
+   let output = 0;
+   const timer = setInterval(() => {
+      result.querySelector('.score').textContent = `${output}%`;
+      if (output === score) {
+         clearInterval(timer);
+      } else {
+         output++;
+      }
+   }, 20);
+});
